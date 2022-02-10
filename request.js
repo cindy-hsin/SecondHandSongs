73f566425817de215be9dae545e41cf1b1d5c5a9
CHUANHSIN CHEN <cindychen@CHUANHSINdeMacBook-Pro.local>
2022-02-09 18:25:47 -0800

    Pushed files

---
+++ request.js
@@ -0,0 +1,50 @@
+// information to reach API
+const url = 'https://secondhandsongs.com/search/performance?';
+// Look for the song 'Lucky', sung by 'Jason Mraz'
+const songTitle =  'lucky';
+const artistName = 'jason%20mraz';
+const queryString = 'op_title=contains&' + `title=${songTitle}&` + 
+'op_performer=contains&' +  `performer=${artistName}`; 
+// op_title and op_performer are types of relations that defines the search
+// op_title has 3 values: contains, starts with, equals;
+// op_performer has 4 values: contains, equals, is set, is not set; 
+// See example: https://secondhandsongs.com/search/performance?display=title.performer&op_title=contains&title=lucky&op_performer=contains&performer=jason%20mraz&sort=simplifiedTitle
+
+
+// Selecting the submit buttom on the page
+const submit = document.querySelector('#submit');
+
+// AJAX function
+const getSuggestions = async () => {
+  const endpoint = url + queryString;
+  const option = {
+    method: 'GET',
+    mode: 'no-cors',
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+
+  try{
+    console.log("before fetch");
+    const response =  await fetch(endpoint, option);
+    console.log("after fetch")
+    console.log("Print response.ok:", response.ok);
+    if(response.ok){
+      let jsonResponse = await response.json();
+      console.log(jsonResponse);
+    }
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+// display results to console
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
+
