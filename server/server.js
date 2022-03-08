1940c0536a7c425e74be6c549aa55cd7ebfddbf2
CHUANHSIN CHEN <cindychen@CHUANHSINdeMacBook-Pro.local>
2022-03-08 15:51:42 -0800

    Completed Data pipeline: Read from songs.csv, query data from API, write into result.csv(cover data)

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-
 const express = require('express');
 const app = express();				// Instantiate an Express application.
 const axios = require('axios');
 
-
+const fs = require('fs');
+const {parse} = require('csv-parse');
+const {stringify} = require('csv-stringify');
+const {transform} = require('stream-transform');
 
 /**
  * songsWithArtistName.csv
@@ -26,82 +28,131 @@ const axios = require('axios');
 
 
 /** A global list of cover record objects, which will eventually be written into csv file.*/
-let coverRecords = [];
+// const coverRecords = [];
 
-/**  
- * Returns a "Cover" objects: 
- * {coverName: "", performerName: "", songId: "", spotifyTrackUrl: "", youtubeUrl: ""};
- * Input: 
-*/
-function buildCoverRecord() {
-  let cover = {};
+startProcess();
+async function startProcess() {
+  const readStream = fs.createReadStream('/Users/cindychen/Documents/NEU/Course_Material/cs5200/Project/second_hand_songs/testData/songsWithArtistName.csv');
+  const parser = readStream.pipe(parse({delimiter: ',', columns: [
+    "SongId", "SongName", "SpotifyId", "ArtistId", "AlbumId", "ArtistName"
+  ], from_line:2, to_line: 3}));    // Add options!!
+  
 
+  // Transformer: parser reads in all columns. Transformer can help filter out unwanted columns.
+  const transformer = parser.pipe(transform(function (data) {
+    console.log(data);
+    return {SongId: data['SongId'], SongName: data['SongName'], ArtistName: data['ArtistName']};  // lacking artistName
+  }))
 
-  return cover;
+  const stringifier = stringify({header: true, 
+    columns: ['CoverName','PerformerName','YoutubeUri', 'SongId', 'SongName', 'ArtistName'],quoted: true, quoted_empty: true}); //?'SongName', 'ArtistName' are duplicate info. Should be dropped
+
+    // Async API calls:
+    const url = 'https://secondhandsongs.com/search/performance?';  
+
+    for await (const record of transformer) { // Each record is a (original) track info
+      // const covers = await new Promise((resolve, reject) => {
+      //   if (false) reject('Error message');
+      //   else resolve([{CoverName: 'lucky', YoutubeUrl: 'https://wertyujhgfdsweaytuijh'},
+      //                 {CoverName: 'lucky2', YoutubeUrl: 'https://wertyujhgfdsweaytuijh'}]);
+      // });
+      const songTitle =  record.SongName;//'lucky';
+      const artistName = record.ArtistName;//'jason%20mraz';     // TODO: Uncomment, after reading source csv has been added the field: "ArtistName"
+      const queryString = 'op_title=contains&' + `title=${songTitle}&` + 
+      'op_performer=contains&' +  `performer=${artistName}`; 
+      const searchTrackEndpoint = url + queryString;
+
+      const coverList = await collectCoverRecords(searchTrackEndpoint);   // Return a list of cover objects, OR null.
+      // Could be null if :
+      // No track found with the query endpoint; OR
+      // No cover found for the given track;
+      if (coverList === null) {
+        console.log(`No cover found for SongTitle=${songTitle}, ArtistName=${artistName}!!`);
+        continue;
+      }
+
+      for (const cover of coverList) {
+        record.CoverName = cover.CoverName;
+        record.PerformerName = cover.PerformerName;
+        record.YoutubeUri = cover.YoutubeUri;
+        stringifier.write(record);
+      }
+
+      // record.CoverName = cover.CoverName;
+      // record.YoutubeUrl = cover.YoutubeUrl;
+      // stringifier.write(record);
+  }
+  const writeStream = fs.createWriteStream('/Users/cindychen/Documents/NEU/Course_Material/cs5200/Project/second_hand_songs/testData/result.csv');
+
+  stringifier.pipe(writeStream);
+  stringifier.end();
 }
 
-
-function appendIntoCoverRecords(cover) {
-
-}
-
-
-
-// CoverId VARCHAR(255) auto_incremented,
-// CoverName 
-// PerformerName
-// SongId
-// SpotifyTrackUrl (link to Spotify UI/webpage to play the track)
-// YoutubeUrl 
+// CoverId INT auto_incremented,
+// CoverName string
+// PerformerName string
+// SongId INT
+// SpotifyTrackUrl (link to Spotify UI/webpage to play the track) string
+// YoutubeUrl  string
 
 
-// Information to reach API
-const url = 'https://secondhandsongs.com/search/performance?';
-// Look for the song 'Lucky', sung by 'Jason Mraz'
-const songTitle =  'dfgfyujn';//'lucky';
-const artistName = 'ijvdssry';//'jason%20mraz';
-const queryString = 'op_title=contains&' + `title=${songTitle}&` + 
-'op_performer=contains&' +  `performer=${artistName}`; 
-const searchTrackEndpoint = url + queryString;
+// // Information to reach API
+// const url = 'https://secondhandsongs.com/search/performance?';
+// // Look for the song 'Lucky', sung by 'Jason Mraz'
+// const songTitle =  'dfgfyujn';//'lucky';
+// const artistName = 'ijvdssry';//'jason%20mraz';
+// const queryString = 'op_title=contains&' + `title=${songTitle}&` + 
+// 'op_performer=contains&' +  `performer=${artistName}`; 
+// const searchTrackEndpoint = url + queryString;
 
-startProcess(searchTrackEndpoint);
 
-async function startProcess(searchTrackEndpoint) {
+
+// collectCoverRecords(searchTrackEndpoint);
+
+
+/**
+ * Wrapper function that collects cover records for each songId from database.
+ * @param {*} searchTrackEndpoint 
+ * @returns 
+ */
+async function collectCoverRecords(searchTrackEndpoint) {
+  const coverList = [];
+
   const trackObj = await requestTrackUriFromQueryEndpoint(searchTrackEndpoint);
   const {trackUri, trackTitle} = trackObj;
   if (trackUri === null) {
     console.log("End Program! No track found with the query endpoint: ", searchTrackEndpoint)
-    return;
+    return null;
   }
 
   const coverUriList = await requestCoverUriListFromTrackUrl(trackUri);
   if (coverUriList.length === 0) {
     console.log("End Program! No cover found for the given track: \n"
     + ` Title: ${trackTitle};  TrackUri: ${trackUri} `);
-    return;
+    return null;
   }
 
-  for (let cover of coverUriList) {
-    await buildSingleCoverRecord(coverUri);
+  for (let coverUri of coverUriList) {
+    const cover = await buildSingleCoverRecord(coverUri);
+    coverList.push(cover);
   }
-
-
+  return coverList;   // Here, coverList must be non-empty.
 }
 
 
 // Functions to call API
-async function requestDataWriteToCsv() {
+// async function requestDataWriteToCsv() {
   
-  const coverUriSequence = await getCoverUriSequence();
-  console.log(typeof(coverUriSequence));
+//   const coverUriSequence = await getCoverUriSequence();
+//   console.log(typeof(coverUriSequence));
 
-  for (let coverUri of coverUriSequence) {
-    const youtubeUri = await buildSingleCoverRecord(coverUri);
-    console.log("**** Youtube Uri:", youtubeUri);
-    // TODO: write each youtubeUri into CSV. (CSV table/Database Table to be designed(i.e. What columns does the DB table contain? 
+//   for (let coverUri of coverUriSequence) {
+//     const youtubeUri = await buildSingleCoverRecord(coverUri);
+//     console.log("**** Youtube Uri:", youtubeUri);
+//     // TODO: write each youtubeUri into CSV. (CSV table/Database Table to be designed(i.e. What columns does the DB table contain? 
 
-  }
-}
+//   }
+// }
 
 
 
@@ -163,48 +214,59 @@ async function requestCoverUriListFromTrackUrl(trackUri) {
 
 // {coverName: "", performerName: "", songId: "", spotifyTrackUrl: "", youtubeUrl: ""};
 // Rename: buildSingleCoverRecord
+
+/**
+ * Returns a single cover object that contains coverName, performerName, 
+ *   songId (database foregin key, referencing a unique record in Song Table), 
+ *   spotifyTrackUrl, youtubeUrl (to be displayed to user)
+ * @param {*} coverUri 
+ * @returns a single cover object; object properties may have null value,
+ * depending on the response data.
+ */
 async function buildSingleCoverRecord(coverUri) {
-  let cover = {
-    coverName:"",
-    performerName: "", 
-    songId: "", 
-    spotifyTrackUrl: "", 
-    youtubeUrl: ""
-  };
+  const cover = {
+    CoverName: null, 
+    PerformerName :null,
+    YoutubeUri: null    //TODO: add SpotifyTrackUri
+  }; // CoverName, YoutubeUrl
 
-  let coverResponse = null; // TODO: Is null an appropriate default value??
-
-  let youtubeUri;
   try {
-    coverResponse = await axios.get(coverUri, {headers: {'Accept': 'application/json'}});
-  } catch(err) {
-    console.log(err);
-  }
-  console.log("**** Cover response:", coverResponse)
-  const coverResponseData = coverResponse.data;
-  console.log("**** Cover response Data:", coverResponseData);
-  // console.log("**** Check if releases is an array", Array.isArray(coverResponseData.releases)); // true
-  // console.log("**** Check if external_uri is an array", Array.isArray(coverResponseData.external_uri));  // true
-  
+    const coverResponse = await axios.get(coverUri, {headers: {'Accept': 'application/json'}});
+    // console.log("**** Cover response:", coverResponse);
+    const coverResponseData = coverResponse.data;
+    console.log("**** Cover response Data:", coverResponseData);
 
-
-
-
-    // TODO: Check if taking [0] element is accurate!!!
+    cover.CoverName = coverResponseData.title;
+    cover.PerformerName = coverResponseData.performer.name;  // TODO: Can coverResponseData.performer be null/empty??
+    
+        // TODO: Check if taking [0] element is accurate!!!
     if (coverResponseData.releases.length > 0) {
       const coverReleaseUrl = coverResponseData.releases[0].uri;
-      // Make another API call to coverReleaseUrl to scrap the SpotifyTrackUrl off of HTML data. 
+      // TODO: Make another API call to coverReleaseUrl to scrap the SpotifyTrackUrl off of HTML data. 
+      // TODO: cover.SpotifyTrackUrl = (result of API call).
     }
 
-    
-    youtubeUri = (coverResponseData.external_uri).map(obj=>obj.uri);
-    // console.log("**** Youtube Uri:", youtubeUri);
-    // console.log(youtubeUri[0] !== undefined);
-    // console.log("**** Check if youtubeUri is an array", Array.isArray(youtubeUri)); // true
+    if (coverResponseData.external_uri.length > 0) {
+      cover.YoutubeUri = ((coverResponseData.external_uri)[0]).uri; // TODO: Check if taking [0] element is accurate!!!
+      console.log("########### CHECK! cover.YoutubeUri: ", cover.YoutubeUri);
+    }
 
+  } catch(err) {
+    console.log("Error when querying to ", coverUri +": "+ err);
+  }
+  // console.log("**** Check if releases is an array", Array.isArray(coverResponseData.releases)); // true
+  // console.log("**** Check if external_uri is an array", Array.isArray(coverResponseData.external_uri));  // true
 
-  if (youtubeUri.length > 0) {return youtubeUri[0];}
-  return null;
+  
+  // console.log("**** Youtube Uri:", youtubeUri);
+  // console.log(youtubeUri[0] !== undefined);
+  // console.log("**** Check if youtubeUri is an array", Array.isArray(youtubeUri)); // true
+    // coverName:"",
+    // performerName: "", 
+    // songId: "",    //==> Not here!! 
+    // spotifyTrackUrl: "", 
+    // youtubeUrl: ""
+  return cover;
 }
 
 
